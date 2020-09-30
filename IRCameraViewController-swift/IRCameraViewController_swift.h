e7f21b77976898f982753b1a12cd4e4e71cb495f
Phil <phil@PhildeMac-mini.engenius.login>
2020-09-30 18:44:37 +0800

    First commit.

---
+++ IRCameraViewController-swift/IRCameraViewController_swift.h
@@ -0,0 +1,19 @@
+//
+//  IRCameraViewController_swift.h
+//  IRCameraViewController-swift
+//
+//  Created by Phil on 2020/9/30.
+//  Copyright © 2020 Phil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IRCameraViewController_swift.
+FOUNDATION_EXPORT double IRCameraViewController_swiftVersionNumber;
+
+//! Project version string for IRCameraViewController_swift.
+FOUNDATION_EXPORT const unsigned char IRCameraViewController_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRCameraViewController_swift/PublicHeader.h>
+
+
